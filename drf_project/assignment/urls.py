42524041e84f5efd6acc3d88cd1f85c6291eaf65
fivedasol <81301003+fivedasol@users.noreply.github.com>
2024-05-05 23:22:09 +0900

    urls.py 주석 추가

--- drf_project/assignment/urls.py
+++ drf_project/assignment/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+# drf_project/urls.py 에다가 assignment/로 연결시켜놔서 postman에서 사용하실때 "assignment/" 써주신 다음에 여기 url 쓰시면 될거같아요!
 urlpatterns = [
     path('post/', views.create_assignment),
     path('submission/<int:pk>', views.create_submission)
